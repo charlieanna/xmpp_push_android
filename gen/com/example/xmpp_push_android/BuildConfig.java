d155e42e193c5f7bbaa23228afd19b62fa3ef756
Charlie <ankothari@gmail.com>
2014-01-14 22:30:41 +0530

    Initial commit

---
+++ gen/com/example/xmpp_push_android/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.xmpp_push_android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
